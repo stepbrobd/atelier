2aa782a849b0faad7b0446cf90136c67b70580fa
June Stepp <git@junestepp.me>
2026-06-13 16:18:53 -0500

    flake: switch from nixpkgs-fmt to nixfmt-tree
    nixpkgs-fmt has long been replaced by nixfmt

--- flake.nix
+++ flake.nix
@@ -1,86 +1,89 @@
 {
-  outputs = inputs: inputs.parts.lib.mkFlake { inherit inputs; } {
-    systems = import inputs.systems;
+  outputs =
+    inputs:
+    inputs.parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
 
-    perSystem = { lib, pkgs, ... }:
-      let
-        python = pkgs.python314;
+      perSystem =
+        { lib, pkgs, ... }:
+        let
+          python = pkgs.python314;
 
-        src =
-          with lib.fileset;
-          toSource {
-            root = ./.;
-            fileset = unions [
-              # code
-              ./src
-              ./tests
-              # meta
-              ./license.txt
-              ./pyproject.toml
-              ./readme.md
-              ./uv.lock
+          src =
+            with lib.fileset;
+            toSource {
+              root = ./.;
+              fileset = unions [
+                # code
+                ./src
+                ./tests
+                # meta
+                ./license.txt
+                ./pyproject.toml
+                ./readme.md
+                ./uv.lock
+              ];
+            };
+
+          workspace = inputs.pypuv.lib.workspace.loadWorkspace { workspaceRoot = src; };
+
+          pythonPackages = (pkgs.callPackage inputs.pyp.build.packages { inherit python; }).overrideScope (
+            lib.composeManyExtensions [
+              inputs.pypbs.overlays.wheel
+              (workspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
+            ]
+          );
+        in
+        {
+          packages.default = pythonPackages.mkVirtualEnv (
+            with (lib.importTOML ./pyproject.toml).project; "${name}-${version}"
+          ) workspace.deps.default;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              nixpkgs-fmt
+              python
+              python.pkgs.venvShellHook
+              ruff
+              ty
+              uv
             ];
+
+            venvDir = "./.venv";
+            preShellHook = "uv venv $venvDir";
+            postShellHook = "uv sync";
+
+            UV_PYTHON = python.interpreter;
+            UV_PYTHON_DOWNLOADS = "never";
+            UV_VENV_CLEAR = true;
+
+            LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [ stdenv.cc.cc ]);
           };
 
-        workspace = inputs.pypuv.lib.workspace.loadWorkspace { workspaceRoot = src; };
-
-        pythonPackages = (pkgs.callPackage inputs.pyp.build.packages { inherit python; }).overrideScope (
-          lib.composeManyExtensions [
-            inputs.pypbs.overlays.wheel
-            (workspace.mkPyprojectOverlay { sourcePreference = "wheel"; })
-          ]
-        );
-      in
-      {
-        packages.default = pythonPackages.mkVirtualEnv
-          (with (lib.importTOML ./pyproject.toml).project; "${name}-${version}")
-          workspace.deps.default;
-
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            nixpkgs-fmt
-            python
-            python.pkgs.venvShellHook
-            ruff
-            ty
-            uv
-          ];
-
-          venvDir = "./.venv";
-          preShellHook = "uv venv $venvDir";
-          postShellHook = "uv sync";
-
-          UV_PYTHON = python.interpreter;
-          UV_PYTHON_DOWNLOADS = "never";
-          UV_VENV_CLEAR = true;
-
-          LD_LIBRARY_PATH = lib.makeLibraryPath (with pkgs; [ stdenv.cc.cc ]);
+          formatter = pkgs.writeShellScriptBin "formatter" ''
+            set -eoux pipefail
+            shopt -s globstar
+            root="$PWD"
+            while [[ ! -f "$root/.git/index" ]]; do
+              if [[ "$root" == "/" ]]; then
+                exit 1
+              fi
+              root="$(dirname "$root")"
+            done
+            pushd "$root" > /dev/null
+            ${lib.getExe pkgs.actionlint} -color
+            ${lib.getExe pkgs.deno} fmt **/*.md **/*.yaml
+            ${lib.getExe pkgs.gitleaks} git --no-banner --pre-commit --staged
+            ${lib.getExe pkgs.nixfmt-tree}
+            ${lib.getExe pkgs.ruff} check --fix --unsafe-fixes --preview .
+            ${lib.getExe pkgs.taplo} format pyproject.toml
+            ${lib.getExe pkgs.ty} check --fix --error all .
+            ${lib.getExe pkgs.uv} run pytest
+            ${lib.getExe pkgs.zizmor} --fix=all .
+            popd
+          '';
         };
-
-        formatter = pkgs.writeShellScriptBin "formatter" ''
-          set -eoux pipefail
-          shopt -s globstar
-          root="$PWD"
-          while [[ ! -f "$root/.git/index" ]]; do
-            if [[ "$root" == "/" ]]; then
-              exit 1
-            fi
-            root="$(dirname "$root")"
-          done
-          pushd "$root" > /dev/null
-          ${lib.getExe pkgs.actionlint} -color
-          ${lib.getExe pkgs.deno} fmt **/*.md **/*.yaml
-          ${lib.getExe pkgs.gitleaks} git --no-banner --pre-commit --staged
-          ${lib.getExe pkgs.nixpkgs-fmt} .
-          ${lib.getExe pkgs.ruff} check --fix --unsafe-fixes --preview .
-          ${lib.getExe pkgs.taplo} format pyproject.toml
-          ${lib.getExe pkgs.ty} check --fix --error all .
-          ${lib.getExe pkgs.uv} run pytest
-          ${lib.getExe pkgs.zizmor} --fix=all .
-          popd
-        '';
-      };
-  };
+    };
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
